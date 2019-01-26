9711397cdeb82dfd8340666d7db44d75e31d40fa
mtdcy.chen <mtdcy.chen@gmail.com>
2019-01-26 17:19:34 +0800

    VTDecoder: code refactor

--- MediaFramework/videotoolbox/VTDecoder.cpp
+++ MediaFramework/videotoolbox/VTDecoder.cpp
@@ -47,7 +47,7 @@
 #define DEBUGV(fmt, ...) do {} while(0)
 #endif
 
-// kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange -> nv12
+// kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange <-> nv12
 #define kPreferredPixelFormat kPixelFormatNV12
 
 // https://www.objc.io/issues/23-video/videotoolbox/
@@ -116,13 +116,13 @@ namespace mtdcy { namespace VideoToolbox {
     // for store unorderred image buffers and sort them
     struct Image {
         Image(CVPixelBufferRef pix, MediaTime& pts, MediaTime& duration) :
-        image(CVPixelBufferRetain(pix)), pts(pts), duration(duration) { }
-        
+            image(CVPixelBufferRetain(pix)), pts(pts), duration(duration) { }
+
         Image(const Image& rhs) :
-        image(CVPixelBufferRetain(rhs.image)), pts(rhs.pts), duration(rhs.duration) { }
-        
+            image(CVPixelBufferRetain(rhs.image)), pts(rhs.pts), duration(rhs.duration) { }
+
         ~Image() { if (image) CFRelease(image); }
-        
+
         CVPixelBufferRef    image;
         MediaTime           pts;
         MediaTime           duration;
@@ -132,29 +132,28 @@ namespace mtdcy { namespace VideoToolbox {
     };
 
     struct VTContext {
-        VTContext() : session(NULL), vt_fmt_desc(NULL),
-        inputEOS(false) { }
+        VTContext() : decompressionSession(NULL), formatDescription(NULL),
+        mInputEOS(false), mLastFramePTS(kTimeBegin) { }
 
         ~VTContext() {
-            if (session) {
-                VTDecompressionSessionInvalidate(session);
-                CFRelease(session);
+            if (decompressionSession) {
+                VTDecompressionSessionInvalidate(decompressionSession);
+                CFRelease(decompressionSession);
             }
-            if (vt_fmt_desc) {
-                CFRelease(vt_fmt_desc);
+            if (formatDescription) {
+                CFRelease(formatDescription);
             }
         }
 
         int32_t width, height;
         ePixelFormat pixel;
-        VTDecompressionSessionRef       session;
-        VTDecompressionOutputCallback   callback;
-        CMVideoFormatDescriptionRef     vt_fmt_desc;
-        //Decoder                         *target;
-        bool                            inputEOS;
+        VTDecompressionSessionRef       decompressionSession;
+        CMVideoFormatDescriptionRef     formatDescription;
 
         Mutex                           mLock;
+        bool                            mInputEOS;
         List<Image>                     mImages;
+        MediaTime                       mLastFramePTS;
     };
 
     static void OutputCallback(void *decompressionOutputRefCon,
@@ -164,13 +163,20 @@ namespace mtdcy { namespace VideoToolbox {
             CVImageBufferRef imageBuffer,
             CMTime presentationTimeStamp,
             CMTime presentationDuration) {
-        DEBUG("status %d, infoFlags %#x, imageBuffer %p, presentationTimeStamp %.3f(s)/%.3f(s)",
-                status, infoFlags, imageBuffer,
+
+        CHECK_NULL(decompressionOutputRefCon);
+        CHECK_NULL(sourceFrameRefCon);  // strong ref to the packet
+        sp<MediaPacket> *packet = static_cast<sp<MediaPacket> * >(sourceFrameRefCon);
+
+        DEBUG("packet %p, status %d, infoFlags %#x, imageBuffer %p, presentationTimeStamp %.3f(s)/%.3f(s)",
+                packet->get(), status, infoFlags, imageBuffer,
                 CMTimeGetSeconds(presentationTimeStamp),
                 CMTimeGetSeconds(presentationDuration));
-        
-        if (imageBuffer == NULL) {
-            INFO("null frame");
+
+        delete packet;
+
+        if (status || imageBuffer == NULL) {
+            ERROR("decode frame failed, st = %d", status);
             return;
         }
 
@@ -338,7 +344,7 @@ namespace mtdcy { namespace VideoToolbox {
                 width,
                 height,
                 videoDecoderSpecification,
-                &vtc->vt_fmt_desc);
+                &vtc->formatDescription);
         if (status) {
             ERROR("create video format desc failed");
             CFRelease(videoDecoderSpecification);
@@ -355,11 +361,11 @@ namespace mtdcy { namespace VideoToolbox {
         callback.decompressionOutputRefCon = vtc.get();
         status = VTDecompressionSessionCreate(
                 NULL,
-                vtc->vt_fmt_desc,
+                vtc->formatDescription,
                 (CFDictionaryRef)videoDecoderSpecification,
                 destinationImageBufferAttributes,
                 &callback,
-                &vtc->session);
+                &vtc->decompressionSession);
 
         CFRelease(videoDecoderSpecification);
         CFRelease(destinationImageBufferAttributes);
@@ -431,17 +437,16 @@ namespace mtdcy { namespace VideoToolbox {
     }
 
     static CMSampleBufferRef createCMSampleBuffer(const sp<VTContext>& vtc,
-            const sp<MediaPacket>& packet)
-    {
+            const sp<MediaPacket>& packet) {
         DEBUG("CMBlockBufferGetTypeID: %#x", CMBlockBufferGetTypeID());
         CMBlockBufferRef  blockBuffer = NULL;
-        CMSampleBufferRef sample = NULL;
+        CMSampleBufferRef sampleBuffer = NULL;
 
         OSStatus status = CMBlockBufferCreateWithMemoryBlock(
-                kCFAllocatorDefault,        // structureAllocator
+                kCFAllocatorDefault,        // structureAllocator -> default allocator
                 const_cast<char*>(packet->data->data()), // memoryBlock
                 packet->data->capacity(),   // blockLength
-                kCFAllocatorNull,           // blockAllocator
+                kCFAllocatorNull,           // blockAllocator -> no deallocation
                 NULL,                       // customBlockSource
                 0,                          // offsetToData
                 packet->data->size(),       // dataLength
@@ -451,101 +456,121 @@ namespace mtdcy { namespace VideoToolbox {
         if (kCMBlockBufferNoErr != status) {
             ERROR("CMBlockBufferCreateWithMemoryBlock failed, error = %d", status);
             return NULL;
-        } else {
-            CMSampleTimingInfo timingInfo[1];
-            timingInfo[0].decodeTimeStamp = CMTimeMake(packet->dts.value, packet->dts.timescale);
-            //timingInfo[0].presentationTimeStamp = kCMTimeInvalid;
-            timingInfo[0].presentationTimeStamp = CMTimeMake(packet->pts.value, packet->pts.timescale);
-            timingInfo[0].duration = kCMTimeInvalid;
-            status = CMSampleBufferCreate(
-                    kCFAllocatorDefault,  // allocator
-                    blockBuffer,          // dataBuffer
-                    TRUE,                 // dataReady
-                    0,                    // makeDataReadyCallback
-                    0,                    // makeDataReadyRefcon
-                    vtc->vt_fmt_desc,     // formatDescription
-                    1,                    // numSamples
-                    1,                    // numSampleTimingEntries
-                    &timingInfo[0],       // sampleTimingArray
-                    0,                    // numSampleSizeEntries
-                    NULL,                 // sampleSizeArray
-                    &sample);
         }
+        CHECK_NULL(blockBuffer);
 
-        if (blockBuffer) CFRelease(blockBuffer);
+        CMSampleTimingInfo timingInfo[1];
+        if (packet->dts != kTimeInvalid) {
+            timingInfo[0].decodeTimeStamp = CMTimeMake(packet->dts.value, packet->dts.timescale);
+        } else {
+            WARN("dts is missing");
+            timingInfo[0].decodeTimeStamp = kCMTimeInvalid;
+        }
+        if (packet->pts != kTimeInvalid) {
+            timingInfo[0].presentationTimeStamp = CMTimeMake(packet->pts.value, packet->pts.timescale);
+        } else {
+            WARN("pts is missing");
+            timingInfo[0].presentationTimeStamp = kCMTimeInvalid;
+        }
+        // FIXME
+        timingInfo[0].duration = kCMTimeInvalid;
+
+        status = CMSampleBufferCreate(
+                kCFAllocatorDefault,    // allocator
+                blockBuffer,            // dataBuffer
+                TRUE,                   // dataReady
+                0,                      // makeDataReadyCallback
+                0,                      // makeDataReadyRefcon
+                vtc->formatDescription, // formatDescription
+                1,                      // numSamples
+                1,                      // numSampleTimingEntries
+                &timingInfo[0],         // sampleTimingArray
+                0,                      // numSampleSizeEntries
+                NULL,                   // sampleSizeArray
+                &sampleBuffer);
+
+        CFRelease(blockBuffer);
 
         if (status) {
             ERROR("CMSampleBufferCreate faled, error = %#x", status);
             return NULL;
         } else {
-            return sample;
+            return sampleBuffer;
         }
     }
 
     status_t VTDecoder::write(const sp<MediaPacket>& input) {
         if (input == NULL) {
             INFO("eos");
-            VTDecompressionSessionFinishDelayedFrames(mVTContext->session);
-            mVTContext->inputEOS = true;
-        } else {
-            DEBUG("queue packet: size %zu, dts %.3f(s)",
-                    input->data->size(),
-                    (double)input->dts / mVTContext->timescale);
-
-            CMSampleBufferRef sample = createCMSampleBuffer(mVTContext, input);
-
-            VTDecodeFrameFlags decodeFlags = kVTDecodeFrame_EnableTemporalProcessing;
-            decodeFlags |= kVTDecodeFrame_1xRealTimePlayback;
-            if (input->flags & kFrameFlagReference) {
-                INFO("reference frame");
-                decodeFlags |= kVTDecodeFrame_DoNotOutputFrame;
-            }
-            VTDecodeInfoFlags infoFlagsOut;
-            /**
-             * NOTE:
-             * kVTDecodeFrame_EnableTemporalProcessing may not working as expected,
-             * if the pts is not set properly.
-             */
-            OSStatus status = VTDecompressionSessionDecodeFrame(
-                    mVTContext->session,
-                    sample,
-                    decodeFlags,    // VTDecodeFrameFlags
-                    NULL,       // sourceFrameRefCon
-                    &infoFlagsOut       // VTDecodeInfoFlags
-                    );
-            DEBUG("decode info flag %#x", infoFlagsOut);
-
-            CFRelease(sample);
-
-            if (status) {
-                ERROR("VTDecompressionSessionDecodeFrame failed, error = %#x", status);
-                return UNKNOWN_ERROR;
-            }
+            VTDecompressionSessionFinishDelayedFrames(mVTContext->decompressionSession);
+            // no need to wait here.
+            mVTContext->mInputEOS = true;
+            return OK;
         }
-        VTDecompressionSessionWaitForAsynchronousFrames(mVTContext->session);
+
+        DEBUG("queue packet %p: size %zu, dts %.3f(s), pts %.3f(s)",
+                input.get(),
+                input->data->size(),
+                input->dts.seconds(),
+                input->pts.seconds());
+
+        CHECK_TRUE(input->dts != kTimeInvalid || input->pts != kTimeInvalid);
+
+        CMSampleBufferRef sampleBuffer = createCMSampleBuffer(mVTContext, input);
+
+        // FIXME:
+        // kVTDecodeFrame_EnableTemporalProcessing is not working as expected.
+        // it may be something wrong with the packet's pts => find out!!!
+        VTDecodeFrameFlags decodeFlags = kVTDecodeFrame_EnableTemporalProcessing;
+        //decodeFlags |= kVTDecodeFrame_EnableAsynchronousDecompression;
+        decodeFlags |= kVTDecodeFrame_1xRealTimePlayback;
+        if (input->flags & kFrameFlagReference) {
+            INFO("reference frame");
+            decodeFlags |= kVTDecodeFrame_DoNotOutputFrame;
+        }
+        VTDecodeInfoFlags infoFlagsOut;
+        /**
+         * NOTE:
+         * kVTDecodeFrame_EnableTemporalProcessing may not working as expected,
+         * if the pts is not set properly.
+         */
+        OSStatus status = VTDecompressionSessionDecodeFrame(
+                mVTContext->decompressionSession,   // VTDecompressionSessionRef
+                sampleBuffer,                       // CMSampleBufferRef
+                decodeFlags,                        // VTDecodeFrameFlags
+                new sp<MediaPacket>(input),         // sourceFrameRefCon
+                &infoFlagsOut                       // VTDecodeInfoFlags
+                );
+        DEBUG("decode info flag %#x", infoFlagsOut);
+
+        CFRelease(sampleBuffer);
+
+        if (status) {
+            ERROR("VTDecompressionSessionDecodeFrame failed, error = %#x", status);
+            return UNKNOWN_ERROR;
+        }
         return OK;
     }
-    
+
     struct PixelBuffer : public Memory {
         PixelBuffer(CVPixelBufferRef ref) : Memory(), pixbuf(CVPixelBufferRetain(ref)) { }
         virtual ~PixelBuffer() { CFRelease(data()); }
-        
+
         virtual void*       data() { return pixbuf; };
         virtual const void* data() const { return pixbuf; };
         virtual size_t      capacity() const { return 1; } ;
         virtual status_t    resize(size_t) { return INVALID_OPERATION; }
-        
+
         CVPixelBufferRef pixbuf;
     };
 
     sp<MediaFrame> createMediaFrame(CVPixelBufferRef pixbuf) {
         sp<MediaFrame> frame = new MediaFrame;
         frame->format = get_pix_format(CVPixelBufferGetPixelFormatType(pixbuf));
-        
-        // TODO: find out this frame is backend by gpu memory or system memory
+
         IOSurfaceRef surface = CVPixelBufferGetIOSurface(pixbuf);
         CHECK_NULL(surface);
-        if (1) {
+        if (surface) {
             frame->data[0]          = new Buffer(new PixelBuffer(pixbuf));
             frame->v.strideWidth    = CVPixelBufferGetWidth(pixbuf);
             frame->v.sliceHeight    = CVPixelBufferGetHeight(pixbuf);
@@ -589,18 +614,18 @@ namespace mtdcy { namespace VideoToolbox {
 
             CVPixelBufferUnlockBaseAddress(pixbuf, kCVPixelBufferLock_ReadOnly);
         }
-        
+
         return frame;
     }
 
     sp<MediaFrame> VTDecoder::read() {
-        if (mVTContext->inputEOS && mVTContext->mImages.empty()) {
+        if (mVTContext->mInputEOS && mVTContext->mImages.empty()) {
             INFO("eos...");
             return NULL;
         }
 
         // for frame reordering
-        if (mVTContext->mImages.size() < 4) {
+        if (mVTContext->mImages.size() < 4 && !mVTContext->mInputEOS) {
             INFO("no frames ready");
             return NULL;
         }
@@ -618,10 +643,16 @@ namespace mtdcy { namespace VideoToolbox {
         out->v.width = mVTContext->width;
         out->v.height = mVTContext->height;
         out->pts = frame.pts;
-        
+
+        if (mVTContext->mLastFramePTS >= frame.pts) {
+            ERROR("last %.3f(s), this %.3f(s)", mVTContext->mLastFramePTS.seconds(), frame.pts.seconds());
+            CHECK_TRUE(mVTContext->mLastFramePTS < frame.pts);
+            mVTContext->mLastFramePTS = frame.pts;
+        }
+
         DEBUG("frame size %zu %.3f(s) => %d x %d => %d x %d",
                 out->data[0]->size(),
-                (double)out->pts / mVTContext->timescale,
+                out->pts.seconds(),
                 out->v.width,
                 out->v.height,
                 out->v.strideWidth,
@@ -632,10 +663,11 @@ namespace mtdcy { namespace VideoToolbox {
 
     status_t VTDecoder::flush() {
         // FIXME: how to flush directly
-        VTDecompressionSessionFinishDelayedFrames(mVTContext->session);
-        VTDecompressionSessionWaitForAsynchronousFrames(mVTContext->session);
+        VTDecompressionSessionFinishDelayedFrames(mVTContext->decompressionSession);
+        VTDecompressionSessionWaitForAsynchronousFrames(mVTContext->decompressionSession);
         AutoLock _l(mVTContext->mLock);
         mVTContext->mImages.clear();
+        mVTContext->mLastFramePTS = kTimeBegin;
         return OK;
     }
 
