9efcfa7edca11fcf035a63f7315f336b5104c09e
mtdcy.chen <mtdcy.chen@gmail.com>
2020-06-18 11:57:01 +0800

    cmake code refactor
    1. add static library
    2. fix error for xcode project
    3. remove gtest code as ABE has one

--- xcode.sh
+++ xcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[ ! -d xcode ] && mkdir xcode 
+[ -d xcode ] && rm -rf xcode 
 
+mkdir xcode && 
 cd xcode
-rm -rf CMakeCache.txt
 cmake -G Xcode -DCMAKE_IGNORE_PATH="/usr/local/lib;/usr/local/include" -DCMAKE_INSTALL_PREFIX=~/Library/Frameworks ..
